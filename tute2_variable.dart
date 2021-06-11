ddb90626ed104facdeda97b31c1dcb99b7bdce96
Nitin <nitinkukreti12@gmail.com>
2021-06-11 09:25:19 +0530

    day 2

---
+++ tute2_variable.dart
@@ -0,0 +1,33 @@
+// Number -> int double
+// strings
+// booleans 
+// List
+// map
+// Runes
+// symblos
+
+void main(){
+  // ingetger
+  // frist way
+  int a=10;
+  // // second way
+  var b=20;
+  print(b);
+
+  print(a);
+
+  // strings
+  // frist way
+  String name="nitin";
+// //  second way
+var myname="nitin";
+  print(name);
+  print(myname);
+
+bool isSunday=false;
+var isSunday1=false;
+
+print(isSunday);
+print(isSunday1);
+
+}
